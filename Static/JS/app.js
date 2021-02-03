2d9033f77edafc425635bdeaadba1f28e7b1f5c1
Solrys <74037270+Solrys@users.noreply.github.com>
2021-02-02 21:22:26 -0800

    building filtered tables

--- Static/JS/app.js
+++ Static/JS/app.js
@@ -17,3 +17,22 @@ function buildTable(data) {
       }
     );
   });
+
+  function handleClick() {
+      let date = d3.select("#datetime").property(#value);
+      let filteredData = tableData;
+      // if-statement syntax
+      if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+
+        // Build table using filtered data
+        buildTable(filteredData);
+}
+      };
+      // Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
